cadd7acdb24a9462d5ae6bfb9dc411f515fab270
charanyareddy26 <charanyareddy646@gmail.com>
2025-07-22 22:41:55 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Mygame from './Mygame';
 import Welcome from './Welcome';
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='charanya'>
     <Routes>
